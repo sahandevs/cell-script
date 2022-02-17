6c50dfcfee3e05321e748400cac28530cceda159
sahandevs <sahandevs@gmail.com>
2022-02-17 17:49:54 +0330

    implement `lex_comment`

--- src/lexer.rs
+++ src/lexer.rs
@@ -20,8 +20,47 @@ pub enum Token<'a> {
     ParClose,
 }
 
-pub fn lex(input: &str) -> Result<Vec<Token>, anyhow::Error> {
-    Ok(vec![])
+pub fn lex(input: &'_ str) -> Result<Vec<Token<'_>>, anyhow::Error> {
+    let mut tokens = Vec::new();
+    let mut chars = input.chars().enumerate().peekable().into_iter();
+
+    while let Some(x) = chars.peek() {
+        let (i, c) = *x; // drop mutable borrow
+        match c {
+            '#' => lex_comment(input, &mut tokens, &mut chars, i)?,
+            c if c.is_whitespace() => {
+                // skip whitespace here
+                chars.next();
+            }
+            _ => {
+                todo!();
+            }
+        }
+    }
+
+    Ok(tokens)
+}
+
+type State = (usize, char);
+
+fn lex_comment<'a, I: Iterator<Item = State>>(
+    input: &'a str,
+    tokens: &mut Vec<Token<'a>>,
+    chars: &mut I,
+    last_i: usize,
+) -> Result<(), anyhow::Error> {
+    let mut offset = 0;
+    while let Some((_, c)) = chars.next() {
+        match c {
+            '\n' => {
+                break;
+            }
+            _ => offset += 1,
+        }
+    }
+    let comment_text = &input[last_i + 1..last_i + offset];
+    tokens.push(Token::Comment(comment_text));
+    Ok(())
 }
 
 #[cfg(test)]
@@ -34,7 +73,7 @@ mod tests {
             #[test]
             fn $name() {
                 let mut tokens = lex($data).unwrap().into_iter();
-
+                println!("{:?}", tokens);
                 $(
                   let token = tokens.next();
 
@@ -51,11 +90,13 @@ mod tests {
     test! {
         test_lex_comment,
         r#"
+#
 # comment 1
    # comment 2
    ### comment
         "#,
         [
+            Comment(""),
             Comment(" comment 1"),
             Comment(" comment 2"),
             Comment("## comment"),
